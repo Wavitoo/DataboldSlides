6c59e15610793a807722ac518353fa272630e377
Wavitoo <114447473+Wavitoo@users.noreply.github.com>
2025-07-18 10:09:00 +0200

    chore(setup): initialize React project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import SlideEditor from "./components/SlideEditor";
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center p-4">
+      <SlideEditor />
+    </div>
+  );
+}
