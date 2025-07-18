8da3d502344f9256c0d5973e4b590f1c903294b3
Wavitoo <114447473+Wavitoo@users.noreply.github.com>
2025-07-18 16:12:49 +0200

    fix(slide_editor): change width to 1200 px to avoid black color

--- src/components/SlideEditor.jsx
+++ src/components/SlideEditor.jsx
@@ -60,7 +60,7 @@ export default function SlideEditor() {
       <Toolbar />
 
       <div className="flex-1 flex items-center justify-center p-4">
-        <div className="aspect-video w-full max-w-[1400px] border border-foreground rounded-md flex p-6 gap-12 bg-background">
+        <div className="aspect-video w-full max-w-[1200px] border border-foreground rounded-md flex p-6 gap-12 bg-background">
           <SlideSidebar
             slides={slides}
             activeIndex={activeIndex}
