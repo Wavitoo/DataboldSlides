5cbfec54f9596c47d5ec4635f79b616939ee001b
Wavitoo <114447473+Wavitoo@users.noreply.github.com>
2025-07-18 15:31:36 +0200

    feat(theme): add dark mode support with next-themes provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { ThemeProvider } from "next-themes";
 import SlideEditor from "./components/SlideEditor";
 
 export default function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center p-4">
-      <SlideEditor />
-    </div>
+    <ThemeProvider attribute="class">
+      <div className="h-screen w-screen flex items-center justify-center p-4">
+        <SlideEditor />
+      </div>
+    </ThemeProvider>
   );
 }
